1e3ba74c51f3df72d984449df0736a3cd219f547
Victor Asmad <victorasmad2@gmail.com>
2020-09-27 14:37:41 -0300

    ADD Distro

--- Practica 3/guia.R
+++ Practica 3/guia.R
@@ -1,4 +1,23 @@
+#Distro normal
+# pnorm trae la probabilidad, el phi(c)
+# qnorm trae el valor del percentil, qnorm(p, mu, sigma)
+# en tal caso, si queremos el valor para el 90 percentil, buscamos
+# qnorm(0.9, 0, 1) y obtenemos el valor mas preciso, caso contrario, buscar
+# valores aproximados en la tabla
 # Ejercicio 6
-X <- pnorm(2.5) - pnorm(-2.5)
+#b
+pnorm(2.5) - pnorm(-2.5)
 
+#c
 1-pnorm(-1.37)
+
+#d
+pnorm()
+
+#e
+pnorm(1.65) - pnorm(-1.65)
+
+1- pnorm(3.09)
+
+# percentil 0.98 de una distro normal
+qnorm(0.98, 0, 1)
